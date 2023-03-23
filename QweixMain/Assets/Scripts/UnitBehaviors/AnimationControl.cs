75dfcfda13efad48cee6c90e7d1bee739522b377
Gavin-Cunningham <120994965+Gavin-Cunningham@users.noreply.github.com>
2023-03-23 11:17:38 -0700

    Demolitionist Idle Animations
    -Implemented Demolitionist Idle Animations
    -Changed the Animation Control script to account for movement speed

--- QweixMain/Assets/Scripts/UnitBehaviors/AnimationControl.cs
+++ QweixMain/Assets/Scripts/UnitBehaviors/AnimationControl.cs
@@ -1,3 +1,18 @@
+/****************************************************************************
+*
+*  File              : AnimationControl.cs
+*  Date Created      : 03/21/2023 
+*  Description       : This script is designed to take care of its things
+*
+*  Programmer(s)     : Gavin Cunningham
+*  Last Modification : 03/23/2023
+*  Additional Notes  : 
+*  External Documentation URL :
+*****************************************************************************
+       (c) Copyright 2022-2023 by MPoweredGames - All Rights Reserved      
+****************************************************************************/
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Animations;
@@ -22,9 +37,15 @@ public class AnimationControl : MonoBehaviour
 
     private void FindMoveDirection()
     {
-        float Xmove = agent.desiredVelocity.x;
-        float Ymove = agent.desiredVelocity.y;
-        controller.SetFloat("XInput", Xmove);
-        controller.SetFloat("YInput", Ymove);
+        float speed = agent.desiredVelocity.magnitude;
+        float xMove = agent.desiredVelocity.x;
+        float yMove = agent.desiredVelocity.y;
+        controller.SetFloat("Speed", speed);
+        if (speed >= 0.1f)
+        {
+            controller.SetFloat("XInput", xMove);
+            controller.SetFloat("YInput", yMove);
+        }
+
     }
 }
